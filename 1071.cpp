fa438286b1d5370548a81cdb154b0e23ddbde0dc
WIZARDCXY <wizard_cxy@hotmail.com>
2014-02-10 14:29:58 +0800

    add 1072

---
+++ 1071.cpp
@@ -0,0 +1,72 @@
+/*************************************************************************
+	> File Name: 1071.cpp
+	> Author: Wizard
+	> Mail: wizard_cxy@hotmail.com 
+	> Created Time: Sun 09 Feb 2014 09:02:30 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+void convert2lower(string &line){
+    
+    int len=line.size();
+    for(int i=0; i<len; i++){
+        line[i]=tolower(line[i]);
+    }
+}
+
+pair<string,int> func(const string &line){
+
+    map<string,int> freq;
+
+    int p1,p2;
+    p1=p2=0;
+
+    int len=line.size();
+
+    for(int i=0; i<=len; i++){
+        
+        if((i<len) && isalnum(line[i])){
+            p2++;
+        } else {
+            if(p2>p1){
+                string subs =line.substr(p1,p2-p1);
+                convert2lower(subs);
+                map<string,int>::iterator it=freq.find(subs);
+
+                if(it == freq.end()){
+                    freq[subs]=1;
+                } else {
+                    freq[subs]+=1;
+                }
+            }
+            p1=p2=i+1;
+        }
+    }
+
+    int largest=0;
+
+    map<string,int>::iterator ptr=freq.begin();
+
+    for(map<string,int>::iterator it = freq.begin(); it!= freq.end(); it++){
+        if( it->second > largest){
+            ptr=it;
+            largest=it->second;
+        }
+    }
+    return *ptr;
+}
+
+
+
+int main(){
+    string str;
+    getline(cin,str);
+    pair<string,int> res=func(str);
+    cout<<res.first<<" "<<res.second<<endl;
+    
+}
---
+++ 1072.cpp
@@ -0,0 +1,128 @@
+/*************************************************************************
+	> File Name: 1072.cpp
+	> Author: Wizard
+	> Mail: wizard_cxy@hotmail.com 
+	> Created Time: Mon 10 Feb 2014 10:26:22 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <sstream>
+using namespace std;
+#define MAX 1015
+#define INT_MAX 0x7fffffff
+#include <cstdio>
+
+int w[MAX][MAX],dist[MAX];
+bool vis[MAX];
+int n,m,k,d;
+
+void dijk(int start){
+
+    for(int i=0; i<=n+m; i++){
+        dist[i]=INT_MAX;
+        vis[i]=0;
+    }
+    
+    dist[start]=0;
+
+    while(1){
+        int min=INT_MAX;
+        int min_idx=-1;
+
+        for(int i=1; i<=n+m; i++){
+            if(!vis[i] && dist[i] <min){
+                min=dist[i];
+                min_idx=i;
+            }
+        }
+
+        if(min_idx == -1) break;
+
+        vis[min_idx]=1;
+
+        for(int i=1; i<=m+n; i++){
+            if(w[min_idx][i] < INT_MAX && i != min_idx){
+                if(dist[i] > (dist[min_idx]+w[min_idx][i])){
+                    dist[i]=dist[min_idx]+w[min_idx][i];
+                }
+            }
+        }
+    }
+}
+
+int getInt(string str){
+
+    stringstream stringStream;
+    int num;
+
+    if(str[0] == 'G'){
+        stringStream<<str.substr(1,str.size()-1);
+        stringStream>>num;
+        num+=n;
+    } else {
+        stringStream<<str;
+        stringStream>>num;
+    }
+    return num;
+}
+
+int main(){
+
+    int gas_station=-1;
+    //freopen("1072.txt","r",stdin);
+
+    cin>>n>>m>>k>>d; //d is max serve range
+
+    for(int i=1; i<=n+m; i++){
+        for(int j=1; j<=n+m; j++){
+            w[i][j]=INT_MAX;
+        }
+    }
+
+    for(int i=1; i<=k; i++){
+        string str1,str2;
+        int weight;
+        cin>>str1>>str2>>weight;
+
+        int v1=getInt(str1);
+        int v2=getInt(str2);
+
+        w[v1][v2]=w[v2][v1]=weight;
+    }
+
+    int ret_sum=-1;
+    int ret_min=-1;
+
+    for(int i=n+1; i<=n+m; i++){
+        dijk(i);
+
+        int min=INT_MAX;
+        int sum=0;
+
+        for(int j=1; j<=n; j++){
+            sum+=dist[j];
+
+            if(dist[j]>d){
+                sum=0;
+                break;
+            }
+
+            if(dist[j]<min){
+                min=dist[j];
+            }
+        }
+
+        if(sum !=0 && ((min>ret_min) || (min==ret_min && sum<ret_sum))){
+            ret_sum=sum;
+            ret_min=min;
+            gas_station=i-n;
+        }
+    }
+
+    if(gas_station != -1){
+        printf("G%d\n%.1lf %.1lf\n",gas_station,ret_min*1.0,ret_sum*1.0/(n*1.0)+1e-10);
+    } else {
+        printf("No Solution\n");
+    }
+}
+
