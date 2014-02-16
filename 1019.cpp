d6f3c7f3151ecac0d95f367c9137db9d096bd723
WIZARDCXY <wizard_cxy@hotmail.com>
2014-02-16 16:23:58 +0800

    update

--- 1019.cpp
+++ 1019.cpp
@@ -1,50 +1,47 @@
 /*************************************************************************
-	> File Name: 1019.cpp
-	> Author: Wizard
-	> Mail: wizard_cxy@hotmail.com 
-	> Created Time: Fri 14 Feb 2014 07:50:08 PM CST
- ************************************************************************/
-#include <iostream>
+	> File Name: 1019_bk.cpp
+    > Author: Wizard
+    > Mail: wizard_cxy@hotmail.com 
+    > Created Time: Fri 14 Feb 2014 08:33:07 PM CST
+    ************************************************************************/
+
+#include<iostream>
 using namespace std;
-#include <cstring>
+#include<vector>
+#include<algorithm>
 
-int main(){
-    int n;
-    int radix;
-    
-    cin>>n>>radix;
 
-    string res;
 
-    int i=0;
+int main()
+{
+    vector<int> v;
+    vector<int> rv;
+    int num;
+    int base;
+    cin>>num;
+    cin>>base;
 
-    if(n==0){
-        cout<<"Yes"<<endl<<0;
-        return 0;
+    while(num)
+    {
+        v.push_back(num%base);
+        num/=base;
     }
 
-    while(n){
-        res[i++]=n%radix+'0';
-        n/=radix;
-    }
-    
-    int j,k;
-    bool flag=1;
+    rv = v;
+    reverse(rv.begin(),rv.end());
+    if(rv==v)
+    cout<<"Yes"<<endl;
+    else
+    cout<<"No"<<endl;
 
-    for(j=0,k=i-1; j<k; j++,k--){
-        if(res[j]!=res[k]){
-            flag=0;
-            break;
-        }
+
+    for(int i=v.size()-1;i>=0;i--)
+    {
+        cout<<v[i];
+        if(i!=0)
+        cout<<" ";
     }
 
-    if(flag){cout<<"Yes"<<endl;}
-    else cout<<"No"<<endl;
-
-    for(int j=i-1; j>=0; j--){
-        cout<<res[j];
-        if(j!=0){
-            cout<<" ";
-        }
-    }
+    if(v.size()==0)
+    cout<<0;
 }
