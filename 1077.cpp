a23546616bbd295a312513833f920ac695558780
wizard <wizardcxy@126.com>
2014-11-08 09:01:05 +0800

    add 1077

---
+++ 1077.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 1077.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 07 Nov 2014 11:32:48 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+#include<csdtio>
+#include<vector>
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+}
