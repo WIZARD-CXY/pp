ae7470e5dbf1444ea44aa363234099bf0b0ed75f
wizard <wizardcxy@126.com>
2014-11-11 12:20:36 +0800

    add more

---
+++ 1049.cpp
@@ -0,0 +1,47 @@
+/*************************************************************************
+> File Name: 1049.cpp
+> Author: 
+> Mail: 
+> Created Time: Mon 10 Nov 2014 11:36:26 PM CST
+************************************************************************/
+#include<iostream>
+using namespace std;
+#include<cstdio>
+//reference http://blog.csdn.net/tiantangrenjian/article/details/19908885
+
+int CountOnes(int n){
+    int count = 0;
+    int factor = 1;
+    int higher,lower,cur;
+    while(n/factor != 0){
+        higher = n/(factor*10); //higher digits
+        lower = n - (n/factor)*factor; //lower digit
+        cur = (n/factor)%10; //current digit
+        switch (cur){
+            case 0:
+            count += higher*factor;
+            break;
+            case 1:
+            count += higher*factor + lower +1;
+            break;
+            default:
+            count += higher*factor + factor;
+
+        }
+        factor = factor * 10;
+
+    }
+
+    return count;
+
+}
+
+int main()
+{
+    freopen("1049.txt","r",stdin);
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",CountOnes(n));
+    return 0;
+}
+
---
+++ 1083.cpp
@@ -0,0 +1,52 @@
+/*************************************************************************
+	> File Name: 1083.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 10 Nov 2014 05:49:27 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+
+struct rec{
+    string name;
+    string id;
+    int grade;
+};
+bool cmp(const rec &a ,const rec &b){
+    return a.grade > b.grade;
+}
+int main(){
+    freopen("1083.txt","r",stdin);
+    int n;
+    scanf("%d",&n);
+
+    vector<rec> records;
+
+    records.resize(n);
+
+    for(int i=0; i<n; i++){
+        
+        cin>>records[i].name>>records[i].id>>records[i].grade;
+    }
+
+    sort(records.begin(),records.end(),cmp);
+    int low,high;
+ 
+    scanf("%d%d",&low,&high);
+    bool empty= true;
+
+    for(int i=0; i<records.size(); i++){
+        if(records[i].grade>=low && records[i].grade<=high){
+            cout<<records[i].name<<" "<<records[i].id<<endl;
+            empty=false;
+        }
+    }
+    if(empty){
+        cout<<"NONE"<<endl;
+    }
+
+}
